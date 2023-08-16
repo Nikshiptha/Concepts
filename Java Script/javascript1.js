63663b167e221f95b1f4567d0164267fdc3c16cf
21MH1A4203 <21mh1a4203@acoe.edu.in>
2023-08-16 11:00:58 +0530

    first

---
+++ Java Script/javascript1.js
@@ -0,0 +1,136 @@
+// var a=10;
+// var b=20;
+// var c=30;
+// document.write("Hello everyone</br>");
+// console.log(b);
+// alert(c);
+// document.write("the value of a is",a);
+// document.write("</br>")
+
+
+// var name="Nikshiptha";
+// name="nikku"
+// document.write(name);
+
+// var name="Nikshiptha";
+// name+=" nikku"
+// document.write(name);
+
+// var name="Nikshiptha";
+// document.write(name.length+"</br>");
+// name="nikku"
+// document.write(name.length+"</br>");
+
+// var a=10;
+// document.write(typeof(a));
+// var a=13.23;
+// document.write(typeof(a));
+
+// var a;
+// document.write(typeof(a)) //number
+
+// var a;
+// a +="nani";
+// document.write(a);  //undefinednani
+
+// var a;
+// a="nani"
+// document.write(a);  //string
+
+// var a= true
+// document.write(typeof(a));  //boolean
+
+// var a= "true"
+// document.write(typeof(a)); //string
+
+// var arr=['Nani','Honey','Nikku']
+// document.write(typeof(arr))      //object
+
+// var arr=['Honey','1','true']
+// document.write(arr);         //Honey 1 true
+
+// var arr=['honey','maa','nikku'];
+// console.log(arr);
+// document.write(arr[1]);      //maa
+
+// var arr=['honey','maa','nikku','nani'];
+// document.write(arr[1]+ " "+arr[3]);     //maa nani
+
+// var obj=[{
+// 	'name':'honey',
+// 	'rollno':4256,
+// 	'college':'acoe',
+// 	'branch':'aiml'
+// },
+// {
+// 	'name':'niri',
+// 	'rollno':4256,
+// 	'college':'acoe',
+// 	'branch':'aiml'
+// },
+// {
+// 	'name':'nikshiptha',
+// 	'rollno':4256,
+// 	'college':'acoe',
+// 	'branch':'aiml'
+// },
+// {
+// 	'name':'honey',
+// 	'rollno':4256,
+// 	'college':'acoe',
+// 	'branch':'aiml'
+// },
+// {
+// 	'name':'nikku',
+// 	'rollno':4203,
+// 	'college':'acoe',
+// 	'branch':'aiml'
+// }]
+// console.log(obj[0].name,obj[1].name);
+// console.log(obj[1]);
+// console.log(obj[0]);
+// console.log(obj[0].branch);
+// for( var i=0;i<obj.length;i++)
+// {
+// 	console.log(obj[i].name,obj[i].rollno,obj[i].college,obj[i].branch)
+// }
+
+//1.Window alert
+// alert("Success")
+
+//2.Confirmation box
+// var c=confirm("Are you sure to delete the record");
+// document.write(c);
+
+//3.Prompt Box
+// var a=prompt("Enter a Value");
+// document.write(a);
+
+//to evaluate ae=rithmetic operations
+// var a=+prompt("Enter a value");
+// var b=+prompt("Enter b value");
+// document.write(a+b);
+
+// var a=prompt("Enter a value");
+// var b=prompt("enter b value");
+// document.write(parseInt(a)+parseInt(b));
+
+// var a=10;
+// a=20;
+// document.write(a);
+// let b=20;
+// //let b=30;//reinitilaize is not possible
+// b=30;
+// document.write(b);
+
+// {
+// 	let c=30;
+// 	document.write(c);
+// }
+// document.write(c);// value will not initilize bcz the value is assigned within the scope.
+
+const pi=3.14;
+document.write(pi);
+document.write("<br>")
+const number=9347301234;
+document.write(number)
